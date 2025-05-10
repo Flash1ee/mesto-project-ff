fb629ed6d19a531bba5c1d2d8fe4a9ef674947ae
Dmitriy Varin <dvvarin@avito.ru>
2025-05-10 22:58:44 +0300

    added card initialization

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+function addCardsToPage() {
+    let cardList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+    initialCards.forEach(element => {
+        const card = newCard(element.name, element.link, deleteCard);
+        cardList.append(card);
+    });
+}
 
-// @todo: Функция создания карточки
+function newCard(title, link, rmFunc) {
+    let card = document.querySelector('#card-template').cloneNode(true).content;
 
-// @todo: Функция удаления карточки
+    card.querySelector('.card__image').src = link;
+    card.querySelector('.card__title').textContent = title;
 
-// @todo: Вывести карточки на страницу
+    const rmCardButton = card.querySelector('.card__delete-button');
+
+    rmCardButton.addEventListener('click', function (event) { 
+        const eventTarget = event.target;
+        rmFunc(eventTarget.parentElement);
+    }); 
+
+    return card;
+}
+
+function deleteCard(card) {
+    card.remove();
+}
+
+addCardsToPage();
